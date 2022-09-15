b1d6c33d5bdb74759fdaa5c3e869bc41c1ece555
Daniil Sviridov <daniil_sviridov@mail.ru>
2022-09-16 00:35:39 +0300

    Add project files.

---
+++ CardStorageService.Data/Client.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CardStorageService.Data
+{
+    [Table("Client")]
+    public class Client
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ClientId { get; set; }
+
+        [Column]
+        [StringLength(255)]
+        public string? Surname { get; set; }
+
+        [Column]
+        [StringLength(255)]
+        public string? FirstName { get; set; }
+
+        [Column]
+        [StringLength(255)]
+        public string? Patronymic { get; set; }
+
+        [InverseProperty(nameof(Cards.Client))]
+        public virtual ICollection<Cards> Card { get; set; } = new HashSet<Cards>();
+    }
+}
